6e9a86df7308e0b448e36244398d856940112917
TT <tobthie@gmail.com>
2023-08-18 17:39:42 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -4,6 +4,8 @@ module "network" {
   source  = "app.terraform.io/Post_Test/network/azurerm"
   version = "3.5.0"
 
+  resource_group_name = "${var.network_resource_group_name}"
+
   address_spaces  = ["10.0.0.0/16", "10.2.0.0/16"]
   subnet_prefixes = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   subnet_names    = ["subnet1", "subnet2", "subnet3"]
@@ -13,7 +15,7 @@ module "network" {
     "subnet2" : ["Microsoft.Sql"],
     "subnet3" : ["Microsoft.Sql"]
   }
-  use_for_each = true
+  # use_for_each = true
   tags = {
     environment = "dev"
     costcenter  = "it"
